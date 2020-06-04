d2c9a61c4a3c0e901d468abe7efc7e0e2df497cb
nishaque <61164463+nishaque@users.noreply.github.com>
2020-06-04 17:05:34 +0300

    Update main.cpp

--- hello_world.cpp/main.cpp
+++ hello_world.cpp/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   std::string name;
-  std::cin>>name;
-  std::cout << "Hello, World from " <<name<<std::endl;
+  std::cin>>name;//reading a name
+  std::cout << "Hello, World from " <<name<<std::endl;//writing a message
   return 0;
 }
